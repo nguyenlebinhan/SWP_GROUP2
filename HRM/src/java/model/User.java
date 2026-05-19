c6c253c439479bae1f0b97abc65510a093e75760
QuanLM <minhquan153452@gmail.com>
2026-05-19 15:32:09 +0700

    Update login with email and logout

--- HRM/src/java/model/User.java
+++ HRM/src/java/model/User.java
@@ -10,6 +10,7 @@ package model;
  */
 public class User {
     private int userId;
+    private String username;
     private String email;
     private String password;
     private String fullName;    
@@ -21,8 +22,9 @@ public class User {
     public User() {
     }
 
-    public User(int userId, String email, String password, String fullName, String dob, String address, String roleName, boolean isTemporaryPassword) {
+    public User(int userId, String username, String email, String password, String fullName, String dob, String address, String roleName, boolean isTemporaryPassword) {
         this.userId = userId;
+        this.username = username;
         this.email = email;
         this.password = password;
         this.fullName = fullName;
@@ -40,6 +42,14 @@ public class User {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
